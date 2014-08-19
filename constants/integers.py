f1c9df80037200ccefb30550d1d6085db29724c0
Ali Oral <alioral@windowslive.com>
2014-08-19 15:38:22 +0300

    changes added

--- constants/integers.py
+++ constants/integers.py
@@ -1,1 +1,4 @@
 API_VERSION = 200
+DEFAULT_CHECK_NUMBER = 2 # The times that tweets will be processed for
+DEFAULT_SLEEP_SECOND = 2 # In order to prevent API issues
+TOP_WORD_COUNT = 4
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from constants import strings, integers, lists
-import twitter
+import twitter, time
 
 import fdb
 fdb.api_version(integers.API_VERSION)
@@ -13,12 +13,15 @@ api = twitter.Api(consumer_key= strings.CONSUMER_KEY,
 def set_value(tr, username, word_dictionary):
   try:
     existing_word_dictionary = tr.get(username)
-    print existing_word_dictionary
     if existing_word_dictionary:
-      for w in word_dictionary:
+      existing_word_dictionary = eval(existing_word_dictionary.value)
+      for w in word_dictionary.keys():
+        if w not in existing_word_dictionary:
+          existing_word_dictionary[w] = 0
         existing_word_dictionary[w] += 1
+      tr.set(username, str(existing_word_dictionary))
     else:
-      tr.set(username, word_dictionary)
+      tr.set(username, str(word_dictionary))
     tr.commit().wait()
     return
   except fdb.FDBError as error:
@@ -37,18 +40,29 @@ def process_tweets(tr, terms = lists.DEFAULT_TERMS):
   users = []
   for t in tweets:
     user_screenname = str(t.user.screen_name)
-    word_dictionary = str(get_word_dictionary(t.text))
+    word_dictionary = get_word_dictionary(t.text)
     users.append(user_screenname)
     set_value(tr, user_screenname, word_dictionary)
   return users
 
+def sort_results_by_value(dictionary):
+  return sorted(dictionary, key= dictionary.get, reverse=True)[:integers.TOP_WORD_COUNT]
+
+def print_db(tr):
+  for key, value in tr['':'\xFF']:
+    result_dictionary = eval(value)
+    result_dictionary = sort_results_by_value(result_dictionary)
+    print 'User: ' + key
+    print 'Top ' + str(integers.TOP_WORD_COUNT) + ' words '
+    print map(lambda x:str(x), result_dictionary)
 
 if __name__ == '__main__':
   tr = fdb.open()
-  # tr.clear_range('', '\xFF') - optional
-  users = process_tweets(tr)
-  for u in users:
-    print 'User: ' + u
-    words = eval(tr[u])
-    for word in words.keys():
-      print 'Term: ' + word + '\tFrequency: ' + str(words[word])
+  number_of_checks = integers.DEFAULT_CHECK_NUMBER
+  tr.clear_range('', '\xFF') # Fresh DB everytime
+  for i in xrange(number_of_checks):
+    print 'Check #' + str(i + 1)
+    users = process_tweets(tr)
+    time.sleep(integers.DEFAULT_SLEEP_SECOND)
+  print 'Results'
+  print_db(tr)
